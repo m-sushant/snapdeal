b86fb754cd4091c19562732c8359f16dde203ddd
m-sushant <ankurmishra41@gmail.com>
2021-12-16 13:49:02 +0530

    scroller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,8 +47,6 @@ export class AppComponent {
     @HostListener('window:scroll')
     checkScroll() {
         
-      // window의 scroll top
-      // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
   
       const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
   
@@ -61,7 +59,7 @@ export class AppComponent {
       }
     }
   
-    // TODO: Cross browsing
+    
     gotoTop() {
       window.scroll({ 
         top: 0, 
