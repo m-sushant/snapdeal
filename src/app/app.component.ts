d8d0acb7f230068485af3aef90a9b46a744b80e3
m-sushant <ankurmishra41@gmail.com>
2021-12-18 00:09:13 +0530

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component,HostListener, ElementRef} from '@angular/core';
-import {AppConfiguration} from './config/app-configuration';
+import { Component, HostListener, ElementRef } from '@angular/core';
+import { AppConfiguration } from './config/app-configuration';
 
 @Component({
   selector: 'app-root',
@@ -7,67 +7,88 @@ import {AppConfiguration} from './config/app-configuration';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:any = 'file-configuration-example';
-  logo:any;
-  tagline:any;
-  impact:any;
-  download:any;
-  gift:any;
-  help:any;
-  sell:any;
-  downloadicon:any;
-  searchicon:any;
-  carticon:any;
-  accounticon:any;
-  searchtxt:any;
+  title: any = 'file-configuration-example';
+  logo: any;
+  tagline: any;
+  impact: any;
+  download: any;
+  gift: any;
+  help: any;
+  sell: any;
+  downloadicon: any;
+  searchicon: any;
+  carticon: any;
+  accounticon: any;
+  searchtxt: any;
   constructor(private appConfig: AppConfiguration) {
     this.title = appConfig.title;
-    this.logo=appConfig.logo;
-    this.tagline=appConfig.tagline;
-    this.impact=appConfig.impact;
-    this.download=appConfig.download;
-    this.gift=appConfig.gift;
-    this.help=appConfig.help;
-    this.sell=appConfig.sell;
-    this.downloadicon=appConfig.downloadicon
-    this.searchicon=appConfig.searchicon;
-    this.carticon=appConfig.carticon;
-    this.accounticon=appConfig.accounticon;
-    this.searchtxt=appConfig.searchtxt;
+    this.logo = appConfig.logo;
+    this.tagline = appConfig.tagline;
+    this.impact = appConfig.impact;
+    this.download = appConfig.download;
+    this.gift = appConfig.gift;
+    this.help = appConfig.help;
+    this.sell = appConfig.sell;
+    this.downloadicon = appConfig.downloadicon
+    this.searchicon = appConfig.searchicon;
+    this.carticon = appConfig.carticon;
+    this.accounticon = appConfig.accounticon;
+    this.searchtxt = appConfig.searchtxt;
     console.log(appConfig.title);
     console.log(appConfig.baseUrl);
   }
-  hover:boolean=false;
-  hoverArea(){
-    this.hover=!this.hover;
+  hover: boolean = false;
+  hoverArea() {
+    this.hover = !this.hover;
+  }
+  isShow: boolean | any;
+  topPosToStartShowing = 100;
+ 
+  click: boolean = false;
+  clickArea() {
+    this.click = !this.click;
+  }
+  click1:boolean|any=false;
+  clickArea1() {
+    this.click1 = !this.click1;
+  }
+
+  @HostListener('window:scroll')
+  checkScroll() {
+
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
     }
-    isShow: boolean|any;
-    topPosToStartShowing = 100;
-  
-    @HostListener('window:scroll')
-    checkScroll() {
-        
-  
-      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-  
-      console.log('[scroll]', scrollPosition);
-      
-      if (scrollPosition >= this.topPosToStartShowing) {
-        this.isShow = true;
-      } else {
-        this.isShow = false;
-      }
-    }
-  
-    
-    gotoTop() {
-      window.scroll({ 
-        top: 0, 
-        left: 0, 
-        behavior: 'smooth' 
-      });
-    }
-  
-  
+  }
+
+
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+
+validateUser(){
+      let signupbox:HTMLElement|any=document.getElementById("signupbox");
+      let loginArea:HTMLElement|any=document.getElementById("signup");
+      signupbox.style.display="none";
+      loginArea.style.display="block";
 }
+
+    invisible(){
+      let login:HTMLElement|any=document.getElementById("overlay");
+      login.style.display="none";
+    }
+}
+
 
